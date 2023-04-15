12aa08e6ea5126edf95ecd749398ad4c98148d2a
Mahmud Bakale <apps.mahmudbakale@gmail.com>
2023-04-15 23:44:28 +0100

    Initials

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+
+        return response()->json($users);
+    }
+
+
+
+    public function register($data)
+    {
+        return response()->json($data);
+    }
+}

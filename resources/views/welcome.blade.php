a552f12d4f13b05424cb756a014c8776f5fe976d
Mahmoud Bakale <mahmoud-bakale@Mahmoud-Bakale-MacBook-Pro.local>
2023-05-02 00:39:09 +0100

    WIP

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -62,37 +62,6 @@
                                 </ul>
                             </div>
                         </div>
-                        <div class="col-lg-4 col-md-12">
-                            <div class="header-top-right">
-                                <ul class="header-top-menu list-style">
-                                    <li><a href="contact.html">Support</a></li>
-                                    <li><a href="contact.html">Help</a></li>
-                                </ul>
-                                <div class="select-lang">
-                                    <i class="ri-global-line"></i>
-                                    <div class="navbar-option-item navbar-language dropdown language-option">
-                                        <button class="dropdown-toggle" type="button" data-bs-toggle="dropdown"
-                                            aria-haspopup="true" aria-expanded="false">
-                                            <span class="lang-name"></span>
-                                        </button>
-                                        <div class="dropdown-menu language-dropdown-menu">
-                                            <a class="dropdown-item" href="#">
-                                                <img src="assets/img/uk.png" alt="flag">
-                                                English
-                                            </a>
-                                            <a class="dropdown-item" href="#">
-                                                <img src="assets/img/china.png" alt="flag">
-                                                简体中文
-                                            </a>
-                                            <a class="dropdown-item" href="#">
-                                                <img src="assets/img/uae.png" alt="flag">
-                                                العربيّة
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
@@ -100,187 +69,41 @@
                 <div class="container">
                     <nav class="navbar navbar-expand-md navbar-light">
                         <a class="navbar-brand" href="index.html">
-                            {{-- <img class="logo-light" src="assets/img/logo.png" alt="logo">
-                            <img class="logo-dark" src="assets/img/logo-white.png" alt="logo"> --}}
-                            <h2>SIS PAY</h2>
+                            <img class="logo-light" src="{{ asset('logo.png') }}" alt="logo" style="width:230px;">
+                            <img class="logo-dark" src="{{asset('logo.png')}}" alt="logo" style="width:230px;">
                         </a>
                         <div class="collapse navbar-collapse main-menu-wrap" id="navbarSupportedContent">
                             <div class="menu-close xl-none">
                                 <a href="javascript:void(0)"> <i class="ri-close-line"></i></a>
                             </div>
                             <ul class="navbar-nav ms-auto">
-                                <li class="nav-item  has-dropdown">
+                                <li class="nav-item">
                                     <a href="#" class="nav-link active">
                                         Home
-                                        <i class="ri-arrow-down-s-line"></i>
                                     </a>
-                                    <ul class="dropdown-menu">
-                                        <li class="nav-item">
-                                            <a href="index.html" class="nav-link">Home One</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="index-2.html" class="nav-link ">Home Two</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="index-3.html" class="nav-link active">Home Three</a>
-                                        </li>
-                                    </ul>
                                 </li>
                                 <li class="nav-item">
                                     <a href="about.html" class="nav-link">
                                         About Us
                                     </a>
                                 </li>
-                                <li class="nav-item has-dropdown">
-                                    <a href="#" class="nav-link">
-                                        Service
-                                        <i class="ri-arrow-down-s-line"></i>
-                                    </a>
-                                    <ul class="dropdown-menu">
-                                        <li class="nav-item">
-                                            <a href="service-one.html" class="nav-link">Service One</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="service-two.html" class="nav-link">Service Two</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="service-details.html" class="nav-link">Service Details</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li class="nav-item has-dropdown">
-                                    <a href="#" class="nav-link">
-                                        Pages
-                                        <i class="ri-arrow-down-s-line"></i>
-                                    </a>
-                                    <ul class="dropdown-menu">
-                                        <li class="nav-item">
-                                            <a href="#" class="nav-link">
-                                                Project
-                                                <i class="ri-arrow-down-s-line"></i>
-                                            </a>
-                                            <ul class="dropdown-menu">
-                                                <li class="nav-item">
-                                                    <a href="project.html" class="nav-link">Our Project</a>
-                                                </li>
-                                                <li class="nav-item">
-                                                    <a href="project-details.html" class="nav-link">Single Project</a>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="team.html" class="nav-link">Our Team</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="open-account.html" class="nav-link">Open Account</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="apply-loan.html" class="nav-link">Apply Loan</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="pricing.html" class="nav-link">Pricing Plan</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="faq.html" class="nav-link">FAQ</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="testimonials.html" class="nav-link">Testimonials</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="#" class="nav-link">
-                                                User Pages
-                                                <i class="ri-arrow-down-s-line"></i>
-                                            </a>
-                                            <ul class="dropdown-menu">
-                                                <li class="nav-item">
-                                                    <a href="my-account.html" class="nav-link">My Account</a>
-                                                </li>
-                                                <li class="nav-item">
-                                                    <a href="login.html" class="nav-link">Login</a>
-                                                </li>
-                                                <li class="nav-item">
-                                                    <a href="register.html" class="nav-link">Register</a>
-                                                </li>
-                                                <li class="nav-item">
-                                                    <a href="recover-password.html" class="nav-link">Recover
-                                                        Password</a>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="terms-of-service.html" class="nav-link">Terms of Service</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="privacy-policy.html" class="nav-link">Privacy Policy</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="error-404.html" class="nav-link">404 Error Page</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li class="nav-item has-dropdown">
-                                    <a href="#" class="nav-link">
-                                        Blog
-                                        <i class="ri-arrow-down-s-line"></i>
-                                    </a>
-                                    <ul class="dropdown-menu">
-                                        <li class="nav-item">
-                                            <a href="#" class="nav-link">
-                                                Blog Layout
-                                                <i class="ri-arrow-down-s-line"></i>
-                                            </a>
-                                            <ul class="dropdown-menu">
-                                                <li class="nav-item">
-                                                    <a href="blog-no-sidebar.html" class="nav-link">Blog Grid</a>
-                                                </li>
-                                                <li class="nav-item">
-                                                    <a href="blog-left-sidebar.html" class="nav-link">Blog Left
-                                                        Sidebar</a>
-                                                </li>
-                                                <li class="nav-item">
-                                                    <a href="blog-right-sidebar.html" class="nav-link">Blog Right
-                                                        Sidebar</a>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="#" class="nav-link">
-                                                Single Blog
-                                                <i class="ri-arrow-down-s-line"></i>
-                                            </a>
-                                            <ul class="dropdown-menu">
-                                                <li class="nav-item">
-                                                    <a href="blog-details-no-sidebar.html" class="nav-link">Blog
-                                                        Details No Sidebar</a>
-                                                </li>
-                                                <li class="nav-item">
-                                                    <a href="blog-details-left-sidebar.html" class="nav-link">Blog
-                                                        Details Left Sidebar</a>
-                                                </li>
-                                                <li class="nav-item">
-                                                    <a href="blog-details-right-sidebar.html" class="nav-link">Blog
-                                                        Details Right Sidebar</a>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </li>
                                 <li class="nav-item">
                                     <a href="contact.html" class="nav-link">Contact Us</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="contact.html" class="nav-link">Sign In</a>
                                 </li>
                                 <li class="nav-item xl-none">
                                     <a href="register.html" class="btn style1">Register Now</a>
                                 </li>
                             </ul>
                             <div class="others-options  lg-none">
-                                <div class="searchbox">
-                                    <input type="search" placeholder="Search">
-                                    <button type="button">
-                                        <i class="flaticon-search"></i>
-                                    </button>
+                                {{-- <div class="header-btn lg-none">
+                                    <a href="register.html" class="btn style1">Sign In</a>
                                 </div>
+                                &nbsp; &nbsp; --}}
                                 <div class="header-btn lg-none">
-                                    <a href="register.html" class="btn style1">Register Now</a>
+                                    <a href="register.html" class="btn style1">Join SISPAY</a>
                                 </div>
                             </div>
                         </div>
@@ -328,7 +151,7 @@
                                     <h1>Fast & Secure Online Money Transfer</h1>
                                     <p>Dea of denouncing pleasure and praising pain was born and lete system, and
                                         expound the ac teachings aitems to sed quia non numquam amet sit dolor.</p>
-                                    <a href="service-one.html" class="btn style1">Get Started</a>
+                                    <a href="service-one.html" class="btn style1">Join SIS PAY</a>
                                 </div>
                             </div>
                             <div class="col-lg-6">
@@ -348,7 +171,7 @@
                                     <h1>Move Your Money In Easy Secured Steps</h1>
                                     <p>Dea of denouncing pleasure and praising pain was born and lete system, and
                                         expound the ac teachings aitems to sed quia non numquam amet sit dolor.</p>
-                                    <a href="service-one.html" class="btn style1">Get Started</a>
+                                    <a href="service-one.html" class="btn style1">SIS PAY</a>
                                 </div>
                             </div>
                             <div class="col-lg-6">
@@ -368,7 +191,7 @@
                                     <h1>Secured &amp; Easy Online Payment Solution </h1>
                                     <p>Dea of denouncing pleasure and praising pain was born and lete system, and
                                         expound the ac teachings aitems to sed quia non numquam amet sit dolor.</p>
-                                    <a href="service-one.html" class="btn style1">Get Started</a>
+                                    <a href="service-one.html" class="btn style1">SIS PAY</a>
                                 </div>
                             </div>
                             <div class="col-lg-6">
@@ -617,7 +440,7 @@
                     <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6">
                         <div class="footer-widget">
                             <a href="index.html" class="footer-logo">
-                                <img src="assets/img/logo-white.png" alt="Image">
+                                <img src="{{asset('logo.png')}}" alt="Image" style="width:230px;">
                             </a>
                             <p class="comp-desc">
                                 On the other hand, we denounce whteous indig nation and dislike men wh beguiled moraized
@@ -750,8 +573,7 @@
                 </div>
             </div>
             <div class="copyright-text">
-                <p> <i class="ri-copyright-line"></i>2021 Raxa. All Rights Reserved By <a
-                        href="https://hibootstrap.com/" target="_blank">HiBootstrap</a></p>
+                <p> <i class="ri-copyright-line"></i>{{date('Y')}} SIS PAY. All Rights Reserved </p>
             </div>
         </footer>
 
